bb4e6d3c1f3892f68327a9c22f39e00dddcc74b3
Sebastian Wagner <sebix@sebix.at>
2018-02-26 20:47:23 +0100

    New project structure

--- packaging_utils/anitya/check_anitya.py
+++ packaging_utils/anitya/check_anitya.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python3
-
+"""
+Checks anitya (release-monitoring.org) for updates.
+"""
+import argparse
 import re
-import requests
 import subprocess
 import sys
+from typing import Optional
+
+import requests
 import tabulate
+
+from ..common import config
 
 PROGRAMS = ['tcpflow', 'ddrescueview', 'html-xml-utils', 'fred', 'H2rename', 'chntpw']
 ANITYA_SEARCH_URL = 'https://release-monitoring.org/projects/search/?pattern=%s'
 ANITYA_PROJECT_REGEX = 'https://release-monitoring.org/project/([0-9]+)'
 
 
-def anitya_find_project_id(proj_name):
+def anitya_find_project_id(proj_name: str) -> Optional[int]:
+    """
+    Returns the anitya project ID with given name.
+    """
     search_url = ANITYA_SEARCH_URL % proj_name
     search = requests.get(search_url)
     if search.url == search_url:
         print("No exact match found for %s." % proj_name, file=sys.stderr)
         return None
-    else:
-        return int(re.match(ANITYA_PROJECT_REGEX, search.url).groups()[0])
+    return int(re.match(ANITYA_PROJECT_REGEX, search.url).groups()[0])
 
-def main():
+def iter_projects(*projects):
+    """
+    Iterates over a list of projects and compares versions.
+    """
     versions = []
-    for prog_name in PROGRAMS:
+    for prog_name in projects:
         prog_id = anitya_find_project_id(proj_name=prog_name)
         res = requests.get('https://release-monitoring.org/project/%d/' % prog_id)
         res.raise_for_status()
@@ -40,6 +52,23 @@ def main():
                 break
         versions.append((prog_name, '.'.join(max_version), '.'.join(opensuse_version)))
     print(tabulate.tabulate(versions, headers=('name', 'anitya', 'opensuse')))
+
+def main():
+    """
+    Main program.
+    """
+    parser = argparse.ArgumentParser(description='Check anitya for project versions.')
+    parser.add_argument('projects', type=int, nargs='*',
+                        help='name of projects to check')
+    args = parser.parse_args()
+    if args.projects:
+        iter_projects(*args.projects)
+    else:
+        projects = config.read_config('check_anitya')
+        if projects:
+            iter_projects(*projects.splitlines())
+        else:
+            print("No projects got as parameter and nothing configured.")
 
 if __name__ == '__main__':
     main()
---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as handle:
+    README = handle.read()
+
+setup(
+    name='intelmq',
+    version='0.1.0',
+    maintainer='Sebastian Wagner',
+    maintainer_email='sebix@sebix.at',
+    python_requires='>=3.3',
+    install_requires=['requests'],
+    packages=find_packages(),
+    license='ISC',
+    description='My packaging utilites.',
+    long_description=README,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: System :: Archiving :: Packaging',
+    ],
+    keywords='packaging update check anitya release-monitoring opensuse',
+    entry_points={
+        'console_scripts': [
+            'check-anitya = packaging_utils.anitya.check_anitya:main',
+        ],
+    },
+)
