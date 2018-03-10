1d34382cd6396cecf5b6c07cebb33269d09f41a0
Sebastian Wagner <sebix@sebix.at>
2018-03-10 14:58:53 +0100

    More robust version comparisons

--- packaging_utils/common/conversion.py
+++ packaging_utils/common/conversion.py
@@ -11,26 +11,62 @@ def int_safe(value: str) -> Union[str, int]:
     Returns int of string if possible, otherwise the original variable.
     """
     try:
-        return int(value)
+        return IntStr(value)
     except ValueError:
         return value
+
+
+class IntStr(int):
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return str(self) == other
+        return super(IntStr, self).__eq__(other)
+
+    def __ge__(self, other):
+        if isinstance(other, str):
+            return str(self) >= other
+        return super(IntStr, self).__ge__(other)
+
+    def __gt__(self, other):
+        if isinstance(other, str):
+            return str(self) > other
+        return super(IntStr, self).__gt__(other)
+
+    def __le__(self, other):
+        if isinstance(other, str):
+            return str(self) <= other
+        return super(IntStr, self).__le__(other)
+
+    def __lt__(self, other):
+        if isinstance(other, str):
+            return str(self) < other
+        return super(IntStr, self).__lt__(other)
+
+    def __ne__(self, other):
+        if isinstance(other, str):
+            return str(self) != other
+        return super(IntStr, self).__ne__(other)
 
 
 class Version(tuple):
     def __new__(self, value=Optional[str]):
         """
         Returns a tuple of integers and strings of a given string version.
-        
+
         Non-numeric prefixes are removed.
         """
         if isinstance(value, str):
             value = re.sub('^[a-z]*-?', '', value)
         original_value = value
         if isinstance(value, str):
-             value = tuple(int_safe(subval) for subval in value.split('.~'))
+            value = tuple(int_safe(subval) for subval in value.split('.'))
         new_class = super(Version, self).__new__(Version, value)
         new_class.original_value = original_value
         return new_class
 
     def __str__(self) -> str:
         return self.original_value
+
+    def __repr__(self) -> str:
+        retval = super(Version, self).__repr__()
+        return '%s <%r>)' % (retval[:-1], self.original_value)
