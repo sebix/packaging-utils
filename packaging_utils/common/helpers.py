2942f73857bec7b53d885af12cf6ecf0906d84f2
Sebastian Wagner <sebix@sebix.at>
2021-07-16 13:04:42 +0200

    software name detection: also strip 'python3-' prefix

--- packaging_utils/common/helpers.py
+++ packaging_utils/common/helpers.py
@@ -6,9 +6,17 @@ import os
 
 from pathlib import Path
 
+PACKAGE_PREFIXES = \
+    {
+        'python-': 'python',
+        'python3-': 'python',
+    }
+
 
 def detect_softwarename() -> str:
     dirname = Path(os.getcwd()).parts[-1]
-    if dirname.startswith('python-'):
-        dirname = dirname[7:]
+    for prefix in PACKAGE_PREFIXES.keys():
+        if dirname.startswith(prefix):
+            dirname = dirname[len(prefix):]
+            break
     return dirname
